50649422389e0f679b06e45936063495bb87ecdd
BcBray <47345751+BcBray@users.noreply.github.com>
2019-02-14 07:40:15 +0200

    Add files via upload

---
+++ testing1-master/testing1-master/group3c/src/main/java/ac/za/cput/testing/Awe.java
@@ -0,0 +1,10 @@
+package ac.za.cput.testing;
+
+public class Awe
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+	System.out.println("GITHUB CLONING SUCCESS !!!!!!!!!");
+    }
+}
